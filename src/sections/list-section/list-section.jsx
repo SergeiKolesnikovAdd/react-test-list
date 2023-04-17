9fb20208d7178dcbab5c73f127924e867535d8e9
Sergey <entaroartansis@gmail.com>
2023-04-18 01:39:01 +0500

    bug fixes

--- src/sections/list-section/list-section.jsx
+++ src/sections/list-section/list-section.jsx
@@ -5,51 +5,94 @@ import "./list-section.scss";
 import axios from "axios";
 
 export const ListSection = () => {
-  const [data, setData] = useState([]);
+  const [text, setText] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [fetching, setFetching] = useState(true);
   const [totalCount, setTotalCount] = useState(0);
 
   useEffect(() => {
     if (fetching) {
-      console.log("fetching");
-      console.log()
-      setFetching(false);
+      console.log("fetching")
       axios
-        .get(
-          `https://jsonplaceholder.typicode.com/comments?_limit=20_page=${currentPage}`
-        )
-        .then((res) => {
-          setData((data) => data.concat(res.data));
-          setCurrentPage((prevState) => prevState + 1);
-          setTotalCount(res.headers[100]);
+        .get(`https://jsonplaceholder.typicode.com/comments?_limit=20_page=${currentPage}`)
+        .then(response => {
+          setText([...text, ...response.data])
+          setCurrentPage(prevState => prevState + 1)
+          setTotalCount(response.headers[100])
         })
-    }
-  }, [fetching, currentPage]);
-  console.log(data)
-
-  useEffect(() => {
-    const scrollHandler = (e) => {
-      if (
-        e.target.documentElement.scrollHeight -
-          (e.target.documentElement.scrollTop + window.innerHeight) <
-          100 &&
-        data.length < totalCount
-      ) {
-        setFetching(true);
+        .finally(() => setFetching(false))
       }
-    };
-    document.addEventListener("scroll", scrollHandler);
+    
+  }, [fetching])
+
+  useEffect(()=> {
+    document.addEventListener("scroll", scrollHandler)
     return function () {
-      document.removeEventListener("scroll", scrollHandler);
-    };
-  }, []);
+      document.removeEventListener("scroll", scrollHandler)
+    }
+  }, [])
+
+  const scrollHandler = (e) => {
+    if (
+      e.target.documentElement.scrollHeight -
+      (e.target.documentElement.scrollTop + window.innerHeight) < 100 && text.length < totalCount) {
+        setFetching(true)
+      }
+  }
+
+    // useEffect(() => {
+    //   if (fetching) {
+    //     console.log("fetching");
+    //     setFetching(false);
+    //     axios
+    //       .get(
+    //         `https://jsonplaceholder.typicode.com/comments?_limit=20_page=${currentPage}`
+    //       )
+    //       .then((res) => {
+    //         setData((data) => data.concat(res.data));
+    //         setCurrentPage((prevState) => prevState + 1);
+    //         setTotalCount(res.headers[100]);
+    //       });
+    //   }
+    // }, [fetching]);
+
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       `https://jsonplaceholder.typicode.com/comments?_limit=20_page=${currentPage}`
+  //     )
+  //     .then((res) => {
+  //       // setData((data) => data.concat(res.data));
+  //       setData([...data, ...res.data])
+  //       setTotalCount(res.headers[100]);
+  //     });
+  // }, [currentPage, ]);
+  // console.log(data)
+
+  // useEffect(() => {
+  //   const scrollHandler = (e) => {
+  //     if (
+  //       e.target.documentElement.scrollHeight -
+  //         (e.target.documentElement.scrollTop + window.innerHeight) <
+  //         100 &&
+  //       data.length < totalCount
+  //     ) {
+  //       setCurrentPage((prevState) => prevState + 1);
+  //       console.log("pizda")
+  //       // setFetching(true);
+  //     }
+  //   };
+  //   document.addEventListener("scroll", scrollHandler);
+  //   return function () {
+  //     document.removeEventListener("scroll", scrollHandler);
+  //   };
+  // }, []);
 
   return (
     <>
       <section className="list-section">
-        {data.map((item) => (
-          <ListItem name={item.name} email={item.email} key={item.id}/>
+        {text.map((text) => (
+          <ListItem name={text.name} email={text.email} key={text.id}/>
         ))}
       </section>
     </>
